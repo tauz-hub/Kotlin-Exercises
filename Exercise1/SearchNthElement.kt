9c25f6829aaae62778e842322d2032987461b4f9
Tauã Santo <86664818+tauzxd@users.noreply.github.com>
2021-11-06 23:51:50 -0300

    Update SearchNthElement.kt

--- Exercise1/SearchNthElement.kt
+++ Exercise1/SearchNthElement.kt
@@ -6,7 +6,7 @@ class SearchNthElement {
     fun <T> nth(list: List<T>, index: Int): T? = if (list.size >= index) list[index - 1] else null
 
     @Test
-    fun test3() {
+    fun main() {
         nth(listOf(1, 1, 2, 3, 5, 8), 2)
         nth(listOf(1, 1, 2, 3, 5, 8), 5)
         nth(listOf(1, 1, 2, 3, 5, 8), 4)
