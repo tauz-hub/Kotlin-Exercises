b6fbe7483c6e90cbd534016f08db1e0bafc18bd8
Tauã Santo <86664818+tauzxd@users.noreply.github.com>
2021-11-06 23:52:08 -0300

    Update SearchPenultimateElement.kt

--- Exercise1/SearchPenultimateElement.kt
+++ Exercise1/SearchPenultimateElement.kt
@@ -3,11 +3,11 @@ package Exercise1
 import org.junit.Test
 
 class SearchPenultimateElement {
-    // 2- Encontre o penúltimo elemento de uma lista.]
+    //Encontre o penúltimo elemento de uma lista.]
     fun <T> penultimate(list: List<T>): T? = if (list.isNotEmpty() && list.size > 1) list[list.lastIndex - 1] else null
 
     @Test
-    fun test2() {
+    fun main() {
         println(penultimate(listOf(1, 2, 3, 4, 5, 6, 7, 8)))
         println(penultimate(listOf(1, 3, 5, 4)))
         println(penultimate(emptyList<Int>()))
